8ddc03ca98b3a41d150efe789be4c9c06baf2f26
Afroj Satwilkar <afrozsatvilkar2014@gmail.com>
2021-09-30 14:26:13 +0530

    Checking two lines are equal or not

--- LineComparisonProgram.java
+++ LineComparisonProgram.java
@@ -6,7 +6,17 @@ public class LineComparisonProgram {
 
         System.out.println("Welcome to Line Comparison Computation Program");
         int X1=2,Y1=3,X2=6,Y2=8;
-        double distance = Math.sqrt((X2-X1)*(X2-X1) + (Y2-Y1)*(Y2-Y1));
-        System.out.println("Distance between "+"("+X1+","+Y1+"),"+"("+X2+","+Y2+") is "+distance);
+        double distance1 = Math.sqrt((X2-X1)*(X2-X1) + (Y2-Y1)*(Y2-Y1));
+        System.out.println("Distance between "+"("+X1+","+Y1+"),"+"("+X2+","+Y2+") is "+distance1);
+
+        int X3=3,Y3=4,X4=12,Y4=13;
+        double distance2 = Math.sqrt((X4-X3)*(X4-X3) + (Y4-Y3)*(Y4-Y3));
+        System.out.println("Distance between "+"("+X3+","+Y3+"),"+"("+X4+","+Y4+") is "+distance2);
+
+        boolean check = distance1==distance2;
+        if (check)
+            System.out.println("Both Lines are Equal");
+        else
+            System.out.println("Both lines are not Equal");
     }
 }
