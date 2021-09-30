8abfed19d184a5264b535965d6cd643ea2517fc4
Afroj Satwilkar <afrozsatvilkar2014@gmail.com>
2021-09-30 14:06:55 +0530

    Calculate Length of Line

--- LineComparisonProgram.java
+++ LineComparisonProgram.java
@@ -1,9 +1,12 @@
-package com.company.LineComparisonComputation;
+package com.company1.LineComparisonComputation;
 
 public class LineComparisonProgram {
 
     public static void main(String[] args) {
 
         System.out.println("Welcome to Line Comparison Computation Program");
+        int X1=2,Y1=3,X2=6,Y2=8;
+        double distance = Math.sqrt((X2-X1)*(X2-X1) + (Y2-Y1)*(Y2-Y1));
+        System.out.println("Distance between "+"("+X1+","+Y1+"),"+"("+X2+","+Y2+") is "+distance);
     }
 }
