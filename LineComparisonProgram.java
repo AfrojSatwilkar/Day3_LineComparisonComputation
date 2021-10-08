8dc1e013fdf8acd34aad164c0ef686aac1a5710b
Afroj Satwilkar <afrozsatvilkar2014@gmail.com>
2021-10-08 20:16:35 +0530

    Day3 Line Comparison greter than or lesser than

--- LineComparisonProgram.java
+++ LineComparisonProgram.java
@@ -6,15 +6,16 @@ public class LineComparisonProgram {
 
         System.out.println("Welcome to Line Comparison Computation Program");
         int X1=2,Y1=3,X2=6,Y2=8;
-        double distance1 = Math.sqrt((X2-X1)*(X2-X1) + (Y2-Y1)*(Y2-Y1));
+        double distance1 = Math.sqrt(Math.pow((X2-X1),2) + Math.pow((Y2-Y1),2));
         System.out.println("Distance between "+"("+X1+","+Y1+"),"+"("+X2+","+Y2+") is "+distance1);
 
         int X3=3,Y3=4,X4=12,Y4=13;
-        double distance2 = Math.sqrt((X4-X3)*(X4-X3) + (Y4-Y3)*(Y4-Y3));
+        double distance2 = Math.sqrt(Math.pow((X4-X3),2) + Math.pow((Y4-Y3),2));
         System.out.println("Distance between "+"("+X3+","+Y3+"),"+"("+X4+","+Y4+") is "+distance2);
-
-
-        if (distance1==distance2)
+        String distance01 = distance1+"";
+        String distance02 = distance2+"";
+        boolean checkEquality = distance01.equals(distance02);
+        if (checkEquality)
             System.out.println("Both Lines are Equal");
         else if (distance1<distance2)
             System.out.println("Distance1 is smaller than Distance2");
